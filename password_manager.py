0cda46285b53d6c1cef85d1a87c9f6b231c0b33e
lukeryan94 <73416258+lukeryan94@users.noreply.github.com>
2020-10-26 16:05:12 +1100

    Add files via upload

--- password_manager.py
+++ password_manager.py
@@ -1,16 +1,24 @@
 import os.path
 import sys
 
-#check for file/create new file
-def checkSystem():
+#check for file/create new file#
+
+def check_system():
     if os.path.exists("info.txt"):
-        pass
+        print("Passwords available!")
+
+
     else:
         file = open("info.txt", 'w')
         file.close()
+        print("file created!")
 
-#new password in txt file
-def newPass():
+
+# new password in txt file #
+
+
+def new_pass():
+
     file = open("info.txt", 'a')
 
     print()
@@ -29,7 +37,7 @@ def newPass():
 
 
 
-    file.write("~~~~~~~~~~"+(website)+"~~~~~~~~~~\n")
+    file.write("~~~~~~~~~~~"+(website)+"~~~~~~~~~~~\n")
     file.write(usrnm)
     file.write(pwd)
     file.write(web)
@@ -38,41 +46,57 @@ def newPass():
     file.close()
     print("Password Added!")
     print()
-    menu()
 
-def viewPass():
-    file = open('info.txt', 'r')
-    content = file.read()
-    file.close()
-    print(content)
 
-def menu():
-    print("1. System Check")          #Menu
-    print("2. Add New Password")
-    print("3. View Passwords")
-    print("4. Exit")
-    print("5. Help")
-    menu_selection = input("Please choose an item from the menu above: ")
+# view passwords
+def view_pass():
+        file = open('info.txt', 'r')
+        content = file.read()
+        file.close()
+        print(content)
 
-    if menu_selection == "1":         #menu selection
-        checkSystem()
-        menu()
-    elif menu_selection == "2":
-        newPass()
-        menu()
-    elif menu_selection == "3":
-        viewPass()menu()
-    elif menu_selection == "4":
-        print("Ending program... Goodbye!")
-        sys.exit()
-    elif menu_selection == "5":
-        help()
-        menu()
-    else:
-        print("You did not enter a valid response!")
-        menu()
 
-menu()
+PASSCODE = "1234"     # passcode to access
+
+connect = input("Please enter passcode: \n")
+
+while connect != PASSCODE:
+    connect = input("Please enter correct passcode: \n")
+if connect == PASSCODE:       # correct passcode opens menu
+    print("Welcome!")
+    while True:
+        print("1. System Check")  # Menu
+        print("2. Add New Password")
+        print("3. View Passwords")
+        print("4. Exit")
+        print("5. Help")
+        menu_selection = input("Please choose an item from the menu above: ")
+
+        if menu_selection == "1":  # menu selection
+            check_system()
+
+        elif menu_selection == "2":
+            new_pass()
+
+        elif menu_selection == "3":
+            view_pass()
+
+
+        elif menu_selection == "4":
+            print("Ending program... Goodbye!")
+            sys.exit()
+        elif menu_selection == "5":
+            help()
+
+        else:
+            print("You did not enter a valid response!")
+
+
+
+
+
+
+
 
 
 
